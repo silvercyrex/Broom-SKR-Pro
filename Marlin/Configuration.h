c0951be1dde4fd8c3a2c6a5484011f93e891c847
Sina <github@sinarastegar.com>
2021-03-28 18:20:00 -0400

    size changes after repositioning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1127,7 +1127,7 @@
 #define Y_BED_SIZE 219
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -65
+#define X_MIN_POS -77
 #define Y_MIN_POS -8
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
